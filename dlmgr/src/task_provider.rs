398863301762b3b218bbfe42e1d9267424d14346
Richard Stephens <rjst01@gmail.com>
2026-05-11 16:09:39 +0200

    refactor: initial support for multiple task provider impls

--- dlmgr/src/task_provider.rs
+++ dlmgr/src/task_provider.rs
@@ -1,16 +1,25 @@
 use crate::error::DlMgrSetupError;
 use crate::task_builder::DownloadProps;
 use crate::worker::DlWorkerTask;
+use async_trait::async_trait;
 use std::cmp::min;
 use std::sync::{Arc, RwLock};
 use tokio::sync::{OwnedSemaphorePermit, Semaphore};
 
 #[derive(Clone)]
 pub(crate) struct TaskProvider {
-    inner: Arc<TaskProviderInner>,
+    inner: Arc<dyn TaskProviderImpl + Send + Sync + 'static>,
 }
 
-struct TaskProviderInner {
+#[async_trait]
+trait TaskProviderImpl {
+    fn next_task(&self) -> Option<DlWorkerTask>;
+    async fn next_task_throttled(
+        &self,
+    ) -> anyhow::Result<Option<(DlWorkerTask, OwnedSemaphorePermit)>>;
+}
+
+struct ParallelTaskProvider {
     chunk_size: u32,
     content_length: u64,
     task_state: RwLock<TaskState>,
@@ -20,6 +29,40 @@ struct TaskProviderInner {
 #[derive(Default)]
 struct TaskState {
     offset: u64,
+}
+
+#[async_trait]
+impl TaskProviderImpl for ParallelTaskProvider {
+    fn next_task(&self) -> Option<DlWorkerTask> {
+        let mut state_write = self.task_state.write().unwrap();
+        if state_write.offset < self.content_length {
+            let remaining_bytes = self.content_length - state_write.offset;
+            let len = min(remaining_bytes, self.chunk_size as u64);
+            let task = DlWorkerTask {
+                offset: state_write.offset,
+                len,
+            };
+            state_write.offset += len;
+            Some(task)
+        } else {
+            None
+        }
+    }
+    async fn next_task_throttled(
+        &self,
+    ) -> anyhow::Result<Option<(DlWorkerTask, OwnedSemaphorePermit)>> {
+        let next_task = self.next_task();
+        if let Some(next_task) = next_task {
+            let permits = self
+                .cache_space
+                .clone()
+                .acquire_many_owned(next_task.len as u32)
+                .await?;
+            Ok(Some((next_task, permits)))
+        } else {
+            Ok(None)
+        }
+    }
 }
 
 impl TaskProvider {
@@ -39,7 +82,7 @@ impl TaskProvider {
         let cache_space = Arc::new(Semaphore::new(max_buffer_size));
 
         Ok(Self {
-            inner: Arc::new(TaskProviderInner {
+            inner: Arc::new(ParallelTaskProvider {
                 chunk_size: props.chunk_size,
                 content_length,
                 task_state: Default::default(),
@@ -48,35 +91,9 @@ impl TaskProvider {
         })
     }
 
-    pub fn next_task(&self) -> Option<DlWorkerTask> {
-        let mut state_write = self.inner.task_state.write().unwrap();
-        if state_write.offset < self.inner.content_length {
-            let remaining_bytes = self.inner.content_length - state_write.offset;
-            let len = min(remaining_bytes, self.inner.chunk_size as u64);
-            let task = DlWorkerTask {
-                offset: state_write.offset,
-                len,
-            };
-            state_write.offset += len;
-            Some(task)
-        } else {
-            None
-        }
-    }
     pub async fn next_task_throttled(
         &self,
     ) -> anyhow::Result<Option<(DlWorkerTask, OwnedSemaphorePermit)>> {
-        let next_task = self.next_task();
-        if let Some(next_task) = next_task {
-            let permits = self
-                .inner
-                .cache_space
-                .clone()
-                .acquire_many_owned(next_task.len as u32)
-                .await?;
-            Ok(Some((next_task, permits)))
-        } else {
-            Ok(None)
-        }
+        self.inner.next_task_throttled().await
     }
 }
