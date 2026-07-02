a584d80ece82774dfbb151c2840bcf710708898c
Richard Stephens <rjst01@gmail.com>
2026-07-02 17:13:33 +0200

    builder for atomic file consumer that doesn't require knowing the hash

--- dlmgr/src/consumers/atomic_file_consumer_sha256.rs
+++ dlmgr/src/consumers/atomic_file_consumer_sha256.rs
@@ -6,11 +6,12 @@ use sha2::{Digest, Sha256};
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::sync::{Mutex, oneshot};
+use tracing::warn;
 
 pub struct AtomicFileConsumerSha256 {
     file: Arc<Mutex<Option<AtomicWriteFile>>>,
     hasher: Sha256,
-    expected_hash: [u8; 32],
+    expected_hash: Option<[u8; 32]>,
     tx: Option<oneshot::Sender<anyhow::Result<()>>>,
 }
 
@@ -35,7 +36,33 @@ impl AtomicFileConsumerSha256 {
             AtomicFileConsumerSha256 {
                 file: Arc::new(Mutex::new(Some(file))),
                 hasher,
-                expected_hash,
+                expected_hash: Some(expected_hash),
+                tx: Some(tx),
+            },
+            rx,
+        ))
+    }
+
+    pub async fn new_unknown_hash(
+        target_file: PathBuf,
+    ) -> Result<
+        (
+            AtomicFileConsumerSha256,
+            oneshot::Receiver<anyhow::Result<()>>,
+        ),
+        Error,
+    > {
+        let file =
+            tokio::task::spawn_blocking(move || AtomicWriteFile::open(target_file)).await??;
+        let hasher = Sha256::new();
+
+        let (tx, rx) = oneshot::channel();
+
+        Ok((
+            AtomicFileConsumerSha256 {
+                file: Arc::new(Mutex::new(Some(file))),
+                hasher,
+                expected_hash: None,
                 tx: Some(tx),
             },
             rx,
@@ -63,13 +90,22 @@ impl SequentialChunkConsumer for AtomicFileConsumerSha256 {
 
     async fn finalise(mut self: Box<Self>) {
         let hash: [u8; 32] = self.hasher.finalize().into();
-        let outcome = if hash == self.expected_hash {
-            commit_file(&self.file).await
-        } else {
-            Err(anyhow!("Hash mismatch: actual={}", hex::encode(hash)))
-        };
+        if let Some(expected_hash) = self.expected_hash
+            && hash != expected_hash
+        {
+            if let Some(tx) = self.tx.take() {
+                tx.send(Err(anyhow!("Hash mismatch: actual={}", hex::encode(hash))))
+                    .ok();
+            }
+            return;
+        }
+        let outcome = commit_file(&self.file).await;
         if let Some(tx) = self.tx.take() {
             tx.send(outcome).ok();
+        } else {
+            warn!(
+                "atomic_file_consumer finalise but completion channel missing. outcome={outcome:?}"
+            );
         }
     }
 
