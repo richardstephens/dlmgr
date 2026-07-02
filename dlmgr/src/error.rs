82c6f0a5347886a41556e94cd11970508a4e3354
Richard Stephens <rjst01@gmail.com>
2026-07-02 10:46:30 +0200

    style: spacing between error variants

--- dlmgr/src/error.rs
+++ dlmgr/src/error.rs
@@ -13,16 +13,22 @@ pub enum TaskBuilderError {
 pub enum DlMgrSetupError {
     #[error("HEAD request to url={0} failed: {1}")]
     HeadRequestFailed(Url, reqwest::Error),
+
     #[error("Server does not support range requests")]
     RangeRequestsUnsupported,
+
     #[error("NoContentLengthHeader")]
     NoContentLengthHeader,
+
     #[error("InconsistentContentLength")]
     InconsistentContentLength,
+
     #[error("InvalidMaxBufferSize")]
     InvalidMaxBufferSize,
+
     #[error("ReqwestClientBuildError: {0}")]
     ReqwestClientBuildError(reqwest::Error),
+
     #[error("Not implemented: {0}")]
     NotImplemented(&'static str),
 }
@@ -32,10 +38,13 @@ pub enum DlMgrSetupError {
 pub enum DlMgrCompletionError {
     #[error("Completion handle unexpectedly dropped. This is probably a bug.")]
     CompletionHandleDropped,
+
     #[error("ReqwestClientBuildError: {0}")]
     ReqwestClientBuildError(reqwest::Error),
+
     #[error("Download task failed: {0}")]
     TaskFailed(#[source] DownloadWorkerError),
+
     #[error("Download task panicked: {0}")]
     TaskPanicked(#[source] tokio::task::JoinError),
 }
@@ -45,8 +54,10 @@ pub enum DlMgrCompletionError {
 pub enum DownloadWorkerError {
     #[error("Worker {0} too many consecutive non-fatal failures, giving up. Last error: {1}")]
     TooManyConsecutiveFailures(u8, reqwest::Error),
+
     #[error("Worker {0} encountered fatal error: {1}")]
     Fatal(u8, #[source] RequestChunkError),
+
     #[error("Re-order chunks error: {0}")]
     ReorderChunks(#[source] ReorderChunkError),
 }
@@ -56,10 +67,13 @@ pub enum DownloadWorkerError {
 pub enum RequestChunkError {
     #[error(transparent)]
     Reqwest(#[from] reqwest::Error),
+
     #[error("SubmitChunkError")]
     SubmitChunkError,
+
     #[error("SplitPermitError: {0}")]
     SplitPermitError(String),
+
     #[error(
         "Received excess bytes from url={url}. task.offset={wtask_offset} task.len={wtask_len} offset={offset} len={len} received_len={received_len}"
     )]
@@ -78,14 +92,19 @@ pub enum RequestChunkError {
 pub enum ReorderChunkError {
     #[error("Chunk consumer failed to consume bytes: {0}")]
     ChunkConsumer(#[source] anyhow::Error),
+
     #[error("next_offset={0} ahead of furthest_offset={1}")]
     NextOffsetAheadOfFurthestOffset(u64, u64),
+
     #[error("Received chunk with offset={0} when we've already moved on. next_offset={1}")]
     ReceivedUnexpectedChunk(u64, u64),
+
     #[error("received duplicate chunk at offset {0}")]
     DuplicateChunk(u64),
+
     #[error("Unreachable condition reached")]
     UnreachableCondition,
+
     #[error("Found {0} leftover chunks after completion")]
     LeftoverChunks(usize),
 }
